519f3fd6bc6a4dba9c2717326fa3bbdf1688ca6f
João Mateus <joaomateusabm@yahoo.com.br>
2024-05-19 00:52:58 -0300

    inital commit

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by João Melo on 18/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
